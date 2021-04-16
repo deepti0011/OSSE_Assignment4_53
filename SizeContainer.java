a6ab91ac06eb90f09797f73be256839327cdbb8e
Virat Singh <SVirat@users.noreply.github.com>
2021-04-16 17:44:44 +0530

    Create SizeContainer.java

---
+++ SizeContainer.java
@@ -0,0 +1,17 @@
+public class SizeContainer {
+ 
+  private int size = 0;
+  
+  public SizeContainer(int size) {
+    this.size = size;
+  }
+  
+  public int getSize() {
+    return this.size; 
+  }
+  
+  public void setSize(int size) {
+    this.size = size; 
+  }
+  
+}
