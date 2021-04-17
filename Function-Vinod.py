8501c85279667e52e97c33832dcdaffed1a2c367
Vinodsom <79354334+Vinodsom@users.noreply.github.com>
2021-04-17 12:21:20 +0530

    Add files via upload

---
+++ Function-Vinod.py
@@ -0,0 +1,15 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # range() with one parameter
+for val in range(6):
+    print(val, end='  ')
+print('\n')
+
+# range() with two parameter
+for val in range(5,10):
+    print(val, end='  ')
+print('\n')
+
+# range() with three parameter
+for val in range(0,8,2):
+    print(val, end='  ')
